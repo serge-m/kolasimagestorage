0db982971fa909bdff862236d7202cc1d04824c1
SergeM <sbmatyunin@gmail.com>
2017-07-05 21:02:38 +0000

    added stup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(name='kolasimagestorage',
+      version='0.0.1',
+      description='Abstraction layer for image storage',
+      author='sergem',
+      author_email='sbmatyunin@gmail.com',
+      url='https://serge-m.github.io',
+      install_requires=required,
+      packages=['kolasimagestorage'],
+     )
+
+
